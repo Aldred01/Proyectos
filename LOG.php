ae51c16ea4fbbf35fcb7e69dfb937148ee8811f6
Aldred01 <144654516+Aldred01@users.noreply.github.com>
2023-12-03 12:29:04 -0500

    Add files via upload

---
+++ LOG.php
@@ -0,0 +1,49 @@
+<?php
+// Configuración de la conexión a la base de datos
+$servername = "MySQL";
+$username = "nicolasv";
+$dbname = "user";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("La conexión a la base de datos falló: " . $conn->connect_error);
+}
+
+// Procesar el formulario cuando se envía
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener datos del formulario
+    $correo = $_POST["correo"];
+    $contrasena = $_POST["contrasena"];
+
+    // Consultar la base de datos para verificar el usuario
+    $sql = "SELECT * FROM tu_tabla WHERE correo = '$correo' AND contrasena = '$contrasena'";
+    $result = $conn->query($sql);
+
+    // Verificar si se encontró un usuario
+    if ($result->num_rows > 0) {
+        echo "Inicio de sesión exitoso";
+    } else {
+        echo "Usuario o contraseña incorrectos";
+    }
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
+
+<section class="iniciarsesion" id="iniciarsesion"> 
+    <h1 class="heading">Iniciar Sesión</h1>
+    
+    <div class="row">
+        <div class="form-container">
+            <form method="post" action="">
+                <input type="email" name="correo" placeholder="Correo">
+                <input type="password" name="contrasena" placeholder="Contraseña">
+                <input type="submit" value="Confirmar">
+            </form>
+        </div>
+    </div>
+</section>
---
+++ REG.php
@@ -0,0 +1,41 @@
+<?php
+// Configuración de la conexión a la base de datos
+$host = 'mysql';
+$usuario = 'nicolasv';
+$base_datos = 'user';
+
+// Conectar a la base de datos
+$conexion = new mysqli($host, $usuario, $base_datos);
+
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die('Error al conectar a la base de datos: ' . $conexion->connect_error);
+}
+echo 'Conexión exitosa a la base de datos';
+
+// Obtener los valores del formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+
+    // Verificar si los campos están vacíos
+    if (empty($nombre) || empty($correo)) {
+        echo 'Por favor, completa todos los campos obligatorios.';
+    } else {
+        // Agregar usuario a la base de datos
+        $stmt = $conexion->prepare('INSERT INTO usuarios (nombre, correo) VALUES (?, ?)');
+        $stmt->bind_param('sss', $nombre, $correo);
+
+        if ($stmt->execute()) {
+            echo 'Usuario agregado con éxito. ID: ' . $stmt->insert_id;
+        } else {
+            echo 'Error al agregar usuario: ' . $stmt->error;
+        }
+
+        $stmt->close();
+    }
+}
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
